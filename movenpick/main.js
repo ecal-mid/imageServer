1e5c2f26a3ba7d2bb6341065258b5b2df483b88a
Tibor Udvari <tibor.udvari@icloud.com>
2018-03-03 15:34:29 +0100

    Changes

--- movenpick/main.js
+++ movenpick/main.js
@@ -4,79 +4,132 @@
   // Simple version - one new image
   // Todo - complicated version, more images
 
-  var data = {};
-
   console.log( "ready!" );
-  
+
+
+var containers = {
+    'happy' : $('#container_happy'),
+    'sad': $('#container_sad'),
+    'angry': $('#container_angry')
+};
+
+var anim_functions = {
+    'happy': happy,
+    'angry': angry,
+    'sad': sad
+}
+
   var animDuration = 500;
 
-  //$( ".container" ).translate('100vw',0);
-    /*  $(".container").animateTransform("translate(100vw)", 750, function(){
-  console.log("animation completed after 750ms");
-});*/
-      /*$( ".container" ).animate({ "left": "-100vw" }, 500 );
-      $( ".container" ).animate({ "left": "0" }, 500 );
-      $( ".container" ).animate({ "left": "-200vw" }, 500 );*/
+  var data = [];
 
-      //angry();
+  function checkForNewData(){
+    $.ajax({
+     type: "GET",
+     url: 'data.json',
+     success: function(response){
+         //console.log(response);
+         var new_data = response;
+         // difference(A, B) - returns values present in A but not present in B
+         console.log(response);
+         var new_images = _.differenceWith(new_data, data, _.isEqual).reverse(); // newest last
+         var removed_images = [];
 
-/*
-      $( ".overlay" ).animate({
-        "left": "0vw",
-        "top": "-60vw",
-        'transform' : 'rotate(60deg)'
-      }, {
-        duration: 500,
-        complete: function() {
-          console.log("Finished!");
+        _.each(removed_images, function(d){
+            removeImage(d.file);
+        });
+
+        _.each(new_images, function(d){
+            addImage(d.file, d.type);
+        });
+
+        if (new_images.length < -10){
+            var newest_img_data = new_images[new_images.length - 1];
+            switch (newest_img_data.type){
+            case 'sad':
+                sad();
+            break;
+            case 'happy':
+                happy();
+            break;
+            case 'angry':
+                angry();
+            break;
+            }
         }
-      });
-*/
-      function happy() {
-    /* 120 sad, angry 0, happy -120 */
-        $('.imageoverlay').removeClass('happy-img sad-img angry-img');
-        $('.imageoverlay').addClass('happy-img');
-       
 
-        $( ".container" ).animate({
-          "left": "-100vw"
-        }, {
-          duration: 500,
-          complete: function() {
-            console.log("Finished!");
-          }
-        });
+         data = new_data;
+         console.log('got here');
+         // Remove old stuff
+         // Check if new stuff is there
+         // If it is add it in - start animation for the last one that is added
+     }
+    });
+  }
+
+setInterval(checkForNewData, 1000);
+
+function removeImage(fn){
+    $('#'+fn).remove();
+}
+
+function addImage(fn, type){
+    //console.log('Add image')
+    // Create new image in jquery
+    var img = $('<img />', {
+      id: fn,
+      src: fn,
+      class: 'imagew'
+    });
+
+    //console.log(type);
+    //console.log(containers[type]);
+    img.prependTo(containers[type]);
+}
+
+  function happy() {
+/* 120 sad, angry 0, happy -120 */
+    $('.imageoverlay').removeClass('happy-img sad-img angry-img');
+    $('.imageoverlay').addClass('happy-img');
 
 
-
+    $( ".container" ).animate({
+      "left": "-100vw"
+    }, {
+      duration: 500,
+      complete: function() {
+        console.log("Finished!");
       }
-      function sad() {
-        $('.imageoverlay').removeClass('happy-img sad-img angry-img');
-        $('.imageoverlay').addClass('sad-img');
+    });
+  }
 
-        $( ".container" ).animate({
-          "left": "0"
-        }, {
-          duration: 500,
-          complete: function() {
-            console.log("Finished!");
-          }
-        });
-      }
+  function sad() {
+    $('.imageoverlay').removeClass('happy-img sad-img angry-img');
+    $('.imageoverlay').addClass('sad-img');
 
-      function angry() {
-        $('.imageoverlay').removeClass('happy-img sad-img angry-img');
-        $('.imageoverlay').addClass('angry-img');
-        
-        $( ".container" ).animate({
-          "left": "-200vw"
-        }, {
-          duration: 500,
-          complete: function() {
-            console.log("Finished!");
-          }
-        });
+    $( ".container" ).animate({
+      "left": "0"
+    }, {
+      duration: 500,
+      complete: function() {
+        console.log("Finished!");
       }
+    });
+  }
+
+  function angry() {
+    $('.imageoverlay').removeClass('happy-img sad-img angry-img');
+    $('.imageoverlay').addClass('angry-img');
+
+    $( ".container" ).animate({
+      "left": "-200vw"
+    }, {
+      duration: 500,
+      complete: function() {
+        console.log("Finished!");
+      }
+    });
+  }
     
     //});
 
--- movenpick/watcher.py
+++ movenpick/watcher.py
@@ -8,14 +8,24 @@ import glob
 import json
 
 images_path = 'images'
-limit = 20
+limit = 5
 data_file = 'data.json'
+
+emotions = ['angry', 'sad', 'happy']
+
+def get_mudac_type(fn):
+    for e in emotions:
+        if e in fn:
+            return e
+    return 'unknown'
 
 
 class MyHandler(FileSystemEventHandler):
 
     @staticmethod
     def on_any_event(event):
+        global emotions
+        global limit
         print('Files have changed ...')
         print(images_path)
         files = glob.glob(images_path + "/*.jpg")
@@ -23,14 +33,27 @@ class MyHandler(FileSystemEventHandler):
         files.sort(key=os.path.getmtime, reverse=True)
         print("changed files are")
         print(files)
-        data = dict()
-        data['angry'] = [f for f in files if 'angry' in f][:limit]
-        data['sad'] = [f for f in files if 'sad' in f][:limit]
-        data['happy'] = [f for f in files if 'happy' in f][:limit]
+
+        files = [f[2:] for f in files]
+
+        # map types
+        data = [{'file': f, 'type': get_mudac_type(f)} for f in files]
+        # todo filter length of certain types
+        limited_data = []
+        counts = [0, 0, 0]
+        emotion_counts = dict(zip(emotions, counts))
+        for d in data:
+            if d['type'] in emotions and emotion_counts[d['type']] < limit:
+                limited_data.append(d)
+                emotion_counts[d['type']] += 1
+
+        #data = dict()
+        #data['angry'] = [f for f in files if 'angry' in f][:limit]
+        #data['sad'] = [f for f in files if 'sad' in f][:limit]
+        #data['happy'] = [f for f in files if 'happy' in f][:limit]
 
         with open('data.json', 'w') as outfile:
-            json.dump(data, outfile)
-
+            json.dump(limited_data, outfile)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
